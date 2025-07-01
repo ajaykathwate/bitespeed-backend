600b3f082af954a07983b45f85f8b616d3753cf0
Ajay Kathwate <ajaykathwate13@gmail.com>
2025-07-01 19:24:38 +0530

    chore: initial setup

---
+++ src/main/java/com/bitespeed/task/BitespeedTaskApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitespeedTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitespeedTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitespeed/task/BitespeedTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitespeed.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
